8d3cbcdd347491f730adae20d6d7e21981cbbff0
Aanandakrishnan <aanandakrishnan.d@gogosoon.com>
2023-03-14 23:29:04 +0530

    [Ak] Init new notify project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,6 @@
+// import '@/styles/globals.css'
+import type * as app from 'next/app'
+
+export default function App({ Component, pageProps }: app.AppProps) {
+  return <Component {...pageProps} />
+}
